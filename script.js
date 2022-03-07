4a4324cda48dbca8f4c1ead430fe86c8526e754a
mwaidesu <machariamwai@gmail.com>
2022-03-07 12:57:21 +0300

    Declare variables in JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const date = document.getElementById("date");
+const form = document.getElementById("myForm");
+const gender = document.getElementById("gender");
+
+const akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
