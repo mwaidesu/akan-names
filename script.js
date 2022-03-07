7adad57375b99b848780c98489962b346756cfa3
mwaidesu <machariamwai@gmail.com>
2022-03-07 15:41:50 +0300

    Add 3 variables

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ let maleArray = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 let femaleArray = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
 function findAkanName(){
-  
+  var DATE = document.getElementById("date");
+  var male = document.getElementById("male");
+  var female = document.getElementById("female");
 }
