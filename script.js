5350d71b196b8a3abbe52ad5781fa698fda4b3dc
mwaidesu <machariamwai@gmail.com>
2022-03-07 17:39:35 +0300

    Remove spacing errors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let maleArray = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-let femaleArray = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+let maleArray = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleArray = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 function findAkanName() {
   var DATE = document.getElementById("date");
@@ -7,37 +7,42 @@ function findAkanName() {
   var female = document.getElementById("female");
 
   thisDateArray = DATE.value.split("-");
-  console.log(thisDateArray)
+  console.log(thisDateArray);
 
   var CENTURY = parseInt(thisDateArray[0][0] + thisDateArray[0][1]);
-  console.log(CENTURY)
+  console.log(CENTURY);
 
-  var YEAR = parseInt(thisDateArray[0][2] + thisDateArray[0][3])
-  console.log(YEAR)
+  var YEAR = parseInt(thisDateArray[0][2] + thisDateArray[0][3]);
+  console.log(YEAR);
 
   var MONTH = parseInt(thisDateArray[1]);
-  console.log(MONTH)
+  console.log(MONTH);
 
   var DATE = parseInt(thisDateArray[2]);
-  console.log(DATE)
+  console.log(DATE);
 
-  var day = parseInt((((CENTURY / 4) - 2 * CENTURY - 1) + ((5 * YEAR / 4)) + ((26 * (MONTH + 1) / 10)) + DATE) % 7);
-  console.log(day)
+  var day = parseInt(
+    (CENTURY / 4 -
+      2 * CENTURY -
+      1 +
+      (5 * YEAR) / 4 +
+      (26 * (MONTH + 1)) / 10 +
+      DATE) %
+      7
+  );
+  console.log(day);
 
   if (maleArray[day] === undefined || femaleArray[day] === undefined) {
-    alert("Enter valid date")
+    alert("Enter valid date");
   } else {
-
     if (male.selected === true) {
-      console.log(maleArray[day])
-      document.getElementById('result').textContent = maleArray[day] + " is your Akan Name";
-
-    }
-    
-    else if (female.selected === true) {
-      console.log(femaleArray[day])
-      document.getElementById('result').textContent = femaleArray[day] + " is your Akan Name";
-
+      console.log(maleArray[day]);
+      document.getElementById("result").textContent =
+        maleArray[day] + " is your Akan Name";
+    } else if (female.selected === true) {
+      console.log(femaleArray[day]);
+      document.getElementById("result").textContent =
+        femaleArray[day] + " is your Akan Name";
     }
   }
 }
