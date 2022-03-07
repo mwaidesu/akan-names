4675e90f6e825463506127b7ed11e3fbb8c554d1
mwaidesu <machariamwai@gmail.com>
2022-03-07 13:29:58 +0300

    Add event listener on form

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const gender = document.getElementById("gender");
 
 const akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+form.addEventListener("submit", function (e){
+  e.preventDefault();
+
+
+  let output = "";
+  let thisArray = [];
+  const newGenderValue = gender.value;
+  const newDateValue = date.value;
+
+  const day = new Date(newDateValue);
+  const dayOfWeek = day.getDay();
+
+  if(newGenderValue === male){
+    thisArray = akanMale;
+  }
+  else{
+    thisArray = akanFemale;
+  }
+
+  output = `Your akan name is ${arr[dayOfWeek]}`;
+
+});
