31c607f519287599a089dd0102d010ea9d331065
mwaidesu <machariamwai@gmail.com>
2022-03-07 15:43:46 +0300

    Add 4 variables

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ function findAkanName(){
   var DATE = document.getElementById("date");
   var male = document.getElementById("male");
   var female = document.getElementById("female");
+
+  thisDateArray = DATE.value.split("-");
+
+  console.log(thisDateArray)
+    var CENTURY = parseInt(thisDateArray[0][0] + thisDateArray[0][1]);
+    console.log(CENTURY)
+
+    var YEAR = parseInt(thisDateArray[0][2] + thisDateArray[0][3])
+    console.log(YEAR)
+
+    var MONTH = parseInt(thisDateArray[1]);
+    console.log(MONTH)
+
+    var DATE = parseInt(thisDateArray[2]);
+    console.log(DATE)
 }
