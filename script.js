0ce5a6fb4820d269624b10062084f88182c971aa
mwaidesu <machariamwai@gmail.com>
2022-03-07 15:40:51 +0300

    Add findAkanName() function

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let maleArray = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 let femaleArray = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+function findAkanName(){
+  
+}
